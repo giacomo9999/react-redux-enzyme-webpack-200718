854f31d2c091521bbcb7ca94d79a3308a127f86a
giacomo9999 <gmarchesi888@gmail.com>
2020-07-18 15:44:38 -0400

    Added webpack-dev-server (it works).

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-What it says on the package: React, Redux, Enzyme, and Webpack.
+Pretty much what it says on the package: React, Redux, Enzyme, and Webpack.
+
+To run it: npm start
 
 Sources:  
 https://www.valentinog.com/blog/babel/
--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Form from "./components/Form";
 
-// const wrapper = document.getElementById("container");
-// wrapper ? ReactDOM.render(<Form />, wrapper) : false;
-
 ReactDOM.render(
   <React.StrictMode>
     <Form />
